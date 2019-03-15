f311244ad5700badc581bd086c776421fec42b7b
isrotrip <isrotrip93@gmail.com>
2019-03-15 11:46:37 +0700

    send file to server, and get response with id and imageURL from GCS. And make the client more reactive for canvas also make image to formData

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var getCanvas; // global variable
+
 let app = new Vue({
   el: '#app',
   data: {
@@ -7,7 +9,10 @@ let app = new Vue({
       email: 'email@domain.com',
       phone: '(123) 123 4567',
       theme: 'card-4'
-    }
+    },
+    newData: '',
+    cardFromDatabase: {},
+    currentCanvas: ''
   },
   methods: {
     changeTheme(theme) {
@@ -18,9 +23,35 @@ let app = new Vue({
         allowTaint: true
       })
         .then(function (canvas) {
+          if(this.currentCanvas) {
+            document.body.removeChild(this.currentCanvas);  
+          }
+          this.currentCanvas = canvas
           document.body.appendChild(canvas);
           getCanvas = canvas
         });
+    },
+    submit() {  
+      var imgageData = getCanvas.toDataURL("image/png");
+      // Now browser starts downloading it instead of just showing it
+      this.newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
+      let data = new FormData()
+      data.append('image', dataURItoBlob(this.newData), 'image')
+      axios
+        .post('http://localhost:3000/uploads', data, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        })
+        .then(({ data }) => {
+          this.cardFromDatabase = data;
+        })
+        .catch(({ response }) => {
+          console.log(response);
+        })
+    },
+    download() {
+      $("#btn-Convert-Html2Image").attr("download", "your_pic_name.png").attr("href", this.newData);
     }
   }
 })
